a8245cb4bf6bbf7738eb334f45ea8394342458f8
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-08-09 12:19:21 +0900

    feat: nvim config update

---
+++ lua/atolycs/icons.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.files = {
+  readonly = "",
+}
+
+return M
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,47 @@
+return {
+  -- status line
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local icons = require("atolycs.icons")
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " " .. icons.files.readonly .. " ",
+        }),
+      }
+    end,
+  },
+
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      require("incline").setup({
+        window = {
+          margin = {
+            vertical = 0,
+            horizontal = 1,
+          },
+        },
+
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+}
